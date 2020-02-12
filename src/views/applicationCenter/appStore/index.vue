2b27bbeeaba032035a8338f199b935df0184206d
caoyuanfei <caoyuanfei@zkyda.com>
2020-02-12 14:33:18 +0800

    应用中心-首页-样式初调

--- src/views/applicationCenter/appStore/index.vue
+++ src/views/applicationCenter/appStore/index.vue
@@ -7,7 +7,7 @@
         <li v-for="item in appList" :key="item.id">
           <div class="app-title-box">
             <div class="app-title">
-              <img src="../../../assets/logo.png" alt="" />
+              <img src="../../../assets/img/appStore/log1.png" alt="" />
               <span>{{ item.title }}</span>
             </div>
             <el-button
@@ -21,7 +21,7 @@
           </div>
           <div class="app-describe">
             <p>
-              基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍
+              基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍基本介绍
             </p>
           </div>
         </li>
@@ -65,49 +65,68 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+img,ul,li {
+  margin: 0;
+  padding: 0;
+  border:none;
+}
 .app-store {
   overflow: hidden;
   box-sizing: border-box;
   .app-list {
     box-sizing: border-box;
     & > ul {
-      padding: 10px;
+      // padding: 10px;
       box-sizing: border-box;
       overflow: hidden;
       & > li {
+        background-color: rgb(255, 255, 255);
+        border-radius: 8px;
         width: 48.5%;
-        border: 1px solid black;
+        height: 190px;
+        border: 1px solid rgb(226, 226, 226);
         float: left;
         box-sizing: border-box;
         margin-right: 10px;
-        padding: 10px;
-        height: 150px;
+        padding: 20px;
         margin-bottom: 10px;
         .app-title-box {
           overflow: hidden;
+          border-bottom: 1px solid rgb(226, 226, 226);
+          padding: 10px;
           margin-bottom: 10px;
           .app-title {
             float: left;
             & > img {
-              width: 100px;
+              width: 38px;
+              height: 40px;
               vertical-align: middle;
               margin-right: 20px;
+              vertical-align:middle;
             }
             & > span {
               font-size: 24px;
               font-weight: bold;
+              vertical-align:middle;
+              font-family:PingFangSC-Medium;
+              font-size:18px;
+              color:#404040;
+              text-align:left;
             }
           }
         }
         .app-describe {
           & > p {
-            text-align: left;
-            font-size: 12px;
             line-height: 16px;
             display: -webkit-box;
             -webkit-box-orient: vertical;
             -webkit-line-clamp: 5;
             overflow: hidden;
+            font-family:PingFangSC-Regular;
+            font-size:14px;
+            color:#666666;
+            text-align:left;
+            padding: 10px;
           }
         }
       }
