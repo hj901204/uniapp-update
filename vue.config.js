f771ce24e28b09ed9c11be1f559a7b23bb44b4f8
lihaobo <lihaobo@zkyda.com>
2020-06-23 13:17:13 +0800

    修改谷歌浏览器自动翻译问题

--- vue.config.js
+++ vue.config.js
@@ -35,6 +35,7 @@ module.exports = {
     proxy: {
       '/supplyx': {
         target: 'http://172.21.0.15:8082',
+        // target: 'http://49.233.100.59:8082',
         changeOrigin: true,
         pathRewrite: {
           '^/supplyx': '/supplyx'
