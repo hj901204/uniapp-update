5ecf179fb56c5e234e01e7730b13ea8d141962d0
lihaobo <lihaobo@zkyda.com>
2020-06-30 17:39:23 +0800

    增加正式环境ip和域名

--- vue.config.js
+++ vue.config.js
@@ -34,8 +34,10 @@ module.exports = {
     },
     proxy: {
       '/supplyx': {
-        target: 'http://172.21.0.15:8082',
-        // target: 'http://49.233.100.59:8082',
+        // target: "http://supplyx.goocidata.com:8082",//正式环境域名
+        target: 'http://172.21.16.16:8082', // 正式环境ip
+        // target: "http://172.21.0.15:8082",//测试环境ip
+        // target: 'http://49.233.100.59:8082',//开发环境ip
         changeOrigin: true,
         pathRewrite: {
           '^/supplyx': '/supplyx'
