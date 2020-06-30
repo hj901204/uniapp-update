4c94e8e4885f755667c5b53489cd77d1a81a558b
lihaobo <lihaobo@zkyda.com>
2020-06-30 17:34:53 +0800

    ip

--- vue.config.js
+++ vue.config.js
@@ -34,8 +34,8 @@ module.exports = {
     },
     proxy: {
       '/supplyx': {
-        // target: 'http://172.21.0.15:8082',
-        target: 'http://49.233.100.59:8082',
+        target: 'http://172.21.0.15:8082',
+        // target: 'http://49.233.100.59:8082',
         changeOrigin: true,
         pathRewrite: {
           '^/supplyx': '/supplyx'
