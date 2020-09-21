19c88e0f84f699cc6e1291f54708f62d45549c95
zhanghuili <zhanghuili@goocidata.com>
2020-09-21 10:25:28 +0800

    显示“应用状态”菜单

--- src/router/application.js
+++ src/router/application.js
@@ -70,81 +70,81 @@ const applicationRouter = {
         isOnlyRoute: true, //用来左侧菜单栏隐藏路由的作用
         //icon: "el-icon-my-yingyong"
       },
-      // children: [
-      //   {
-      //     path: "applicationStatus",
-      //     component: () =>
-      //       import(
-      //         "../views/applicationCenter/enterpriseApplications/applicationStatus"
-      //       ),
-      //     name: "applicationStatus",
-      //     meta: {
-      //       title: "应用状态",
-      //       breadcrumbTitle: "应用状态",
-      //       breadcrumb: true,
-      //       path: "/application/enterpriseApplications/applicationStatus"
-      //     },
-      //     children: [
-      //       {
-      //         path: "Renew",
-      //         component: () =>
-      //           import(
-      //             "../views/applicationCenter/enterpriseApplications/applicationStatus/Renew"
-      //           ),
-      //         name: "Renew",
-      //         meta: {
-      //           title: "应用续费",
-      //           breadcrumbTitle: "应用续费",
-      //           breadcrumb: true,
-      //           path:
-      //             "/application/enterpriseApplications/applicationStatus/Renew"
-      //         }
-      //       }
-      //     ]
-      //   },
-      //   {
-      //     path: "applicationSetting",
-      //     component: () =>
-      //       import(
-      //         "../views/applicationCenter/enterpriseApplications/applicationSetting/index.vue"
-      //       ),
-      //     name: "applicationSetting",
-      //     meta: {
-      //       title: "用户设定",
-      //       breadcrumbTitle: "用户设定",
-      //       breadcrumb: true,
-      //       path: "/application/enterpriseApplications/applicationSetting"
-      //     }
-      //   },
-      //   {
-      //     path: "enterpriseCode",
-      //     component: () =>
-      //       import(
-      //         "../views/applicationCenter/enterpriseApplications/enterpriseCode/index.vue"
-      //       ),
-      //     name: "enterpriseCode",
-      //     meta: {
-      //       title: "邀请供应商",
-      //       breadcrumbTitle: "邀请供应商",
-      //       breadcrumb: true,
-      //       path: "/application/enterpriseApplications/enterpriseCode"
-      //     }
-      //   },
-      //   {
-      //     path: "linkSupplier",
-      //     component: () =>
-      //       import(
-      //         "../views/applicationCenter/enterpriseApplications/linkSupplier/index.vue"
-      //       ),
-      //     name: "linkSupplier",
-      //     meta: {
-      //       title: "关联采购商",
-      //       breadcrumbTitle: "关联采购商",
-      //       breadcrumb: true,
-      //       path: "/application/enterpriseApplications/linkSupplier"
-      //     }
-      //   }
-      // ]
+      children: [
+        {
+          path: "applicationStatus",
+          component: () =>
+            import(
+              "../views/applicationCenter/enterpriseApplications/applicationStatus"
+            ),
+          name: "applicationStatus",
+          meta: {
+            title: "应用状态",
+            breadcrumbTitle: "应用状态",
+            breadcrumb: true,
+            path: "/application/enterpriseApplications/applicationStatus"
+          },
+          // children: [
+          //   {
+          //     path: "Renew",
+          //     component: () =>
+          //       import(
+          //         "../views/applicationCenter/enterpriseApplications/applicationStatus/Renew"
+          //       ),
+          //     name: "Renew",
+          //     meta: {
+          //       title: "应用续费",
+          //       breadcrumbTitle: "应用续费",
+          //       breadcrumb: true,
+          //       path:
+          //         "/application/enterpriseApplications/applicationStatus/Renew"
+          //     }
+          //   }
+          // ]
+        },
+        // {
+        //   path: "applicationSetting",
+        //   component: () =>
+        //     import(
+        //       "../views/applicationCenter/enterpriseApplications/applicationSetting/index.vue"
+        //     ),
+        //   name: "applicationSetting",
+        //   meta: {
+        //     title: "用户设定",
+        //     breadcrumbTitle: "用户设定",
+        //     breadcrumb: true,
+        //     path: "/application/enterpriseApplications/applicationSetting"
+        //   }
+        // },
+        // {
+        //   path: "enterpriseCode",
+        //   component: () =>
+        //     import(
+        //       "../views/applicationCenter/enterpriseApplications/enterpriseCode/index.vue"
+        //     ),
+        //   name: "enterpriseCode",
+        //   meta: {
+        //     title: "邀请供应商",
+        //     breadcrumbTitle: "邀请供应商",
+        //     breadcrumb: true,
+        //     path: "/application/enterpriseApplications/enterpriseCode"
+        //   }
+        // },
+        // {
+        //   path: "linkSupplier",
+        //   component: () =>
+        //     import(
+        //       "../views/applicationCenter/enterpriseApplications/linkSupplier/index.vue"
+        //     ),
+        //   name: "linkSupplier",
+        //   meta: {
+        //     title: "关联采购商",
+        //     breadcrumbTitle: "关联采购商",
+        //     breadcrumb: true,
+        //     path: "/application/enterpriseApplications/linkSupplier"
+        //   }
+        // }
+      ]
     },
     // {
     //   path: "rightsManagement",
